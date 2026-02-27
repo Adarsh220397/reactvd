5e966aba35edabab3d065090787ae0578b85ef33
Adarsh220397 <vadarsh22@gmail.com>
2026-02-27 08:54:00 +0530

    base setup 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import AuthProvider from "./providers/authProvider.tsx";
+// import App from "./App.tsx";
+import { AuthProvider } from "./providers/authProvider.tsx";
+import { AppRouter } from "./router.tsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <AppRouter />
+      <Toaster position="top-right" reverseOrder={false} />
     </AuthProvider>
   </StrictMode>,
 );
